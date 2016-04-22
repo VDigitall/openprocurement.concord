14844318f322a21b53fb505630ab5338f2352b65
Roman Kozlovskyi <krzroman@gmail.com>
2016-04-22 18:33:00 +0300

    Version up

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ entry_points = """\
 """
 
 setup(name='openprocurement.concord',
-      version='0.2',
+      version='0.3',
       description="Conflict resolution daemon",
       long_description=README,
       classifiers=[
